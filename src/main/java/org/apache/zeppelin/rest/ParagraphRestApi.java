d748b8e3ed155ec777b4aaaa977275ed93ea6046
fanrw <fanrenwei@cvte.com>
2022-06-29 12:19:53 +0800

    [feature]增加Paragraph重连RemoteInterpreterServer接口

--- src/main/java/org/apache/zeppelin/rest/ParagraphRestApi.java
+++ src/main/java/org/apache/zeppelin/rest/ParagraphRestApi.java
@@ -11,11 +11,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import org.apache.zeppelin.annotation.ZeppelinApi;
 import org.apache.zeppelin.beans.AnalysisResult;
 import org.apache.zeppelin.interpreter.Interpreter;
 import org.apache.zeppelin.interpreter.InterpreterNotFoundException;
+import org.apache.zeppelin.interpreter.ManagedInterpreterGroup;
+import org.apache.zeppelin.interpreter.remote.RemoteInterpreterProcess;
 import org.apache.zeppelin.interpreter.util.SqlSplitter;
 import org.apache.zeppelin.notebook.AuthorizationService;
 import org.apache.zeppelin.notebook.Note;
@@ -27,6 +30,8 @@ import org.apache.zeppelin.rest.exception.NoteNotFoundException;
 import org.apache.zeppelin.rest.exception.ParagraphNotFoundException;
 import org.apache.zeppelin.server.JsonResponse;
 import org.apache.zeppelin.service.AuthenticationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 @Path("/notebook")
@@ -36,6 +41,7 @@ public class ParagraphRestApi extends AbstractRestApi {
     private final Notebook notebook;
     private final AuthorizationService authorizationService;
     private final AuthenticationService authenticationService;
+    private final Logger LOGGER = LoggerFactory.getLogger(ParagraphRestApi.class);
 
     @Inject
     public ParagraphRestApi(
@@ -52,16 +58,9 @@ public class ParagraphRestApi extends AbstractRestApi {
     @GET
     @ZeppelinApi
     @Path("{noteId}/paragraph/{paragraphId}/analysis")
-    public Response getParagraph(@PathParam("noteId") String noteId,
-                                 @PathParam("paragraphId") String paragraphId) throws IOException, InterpreterNotFoundException {
-
-        Note note = notebook.getNote(noteId);
-        checkIfNoteIsNotNull(note, noteId);
-        checkIfUserCanRead(noteId);
-
-        Paragraph paragraph = note.getParagraph(paragraphId);
-        checkIfParagraphIsNotNull(paragraph, paragraphId);
-
+    public Response analysisParagraph(@PathParam("noteId") String noteId,
+                                      @PathParam("paragraphId") String paragraphId) throws IOException, InterpreterNotFoundException {
+        Paragraph paragraph = getParagraph(noteId, paragraphId);
         AnalysisResult analysisResult = new AnalysisResult(ParagraphTextParser.parse(paragraph.getText()));
         analysisResult.getScripts().addAll(new SqlSplitter().splitSql(analysisResult.getScriptText()));
 
@@ -72,6 +71,61 @@ public class ParagraphRestApi extends AbstractRestApi {
         }
 
         return new JsonResponse<>(Response.Status.OK, analysisResult).build();
+    }
+
+    @GET
+    @ZeppelinApi
+    @Path("{noteId}/paragraph/{paragraphId}/reconnect")
+    public Response reconnectParagraph(@PathParam("noteId") String noteId,
+                                       @PathParam("paragraphId") String paragraphId,
+                                       @QueryParam("includeProcess") Boolean reconnectProcess) throws IOException, InterpreterNotFoundException {
+        if (reconnectProcess == null || !reconnectProcess) {
+            LOGGER.info("Recovering paragraph : {}", paragraphId);
+            getParagraph(noteId, paragraphId).recover();
+            return new JsonResponse<>(Response.Status.OK, "OK").build();
+        }
+
+        LOGGER.info("Recovering remote interpreter process and paragraph : {}", paragraphId);
+        Paragraph paragraph = getParagraph(noteId, paragraphId);
+        Interpreter interpreter = paragraph.getBindedInterpreter();
+
+        RemoteInterpreterProcess remoteInterpreterProcess = getRemoteInterpreterProcess(interpreter);
+        if (remoteInterpreterProcess == null || !remoteInterpreterProcess.reconnect()) {
+            LOGGER.info("Failed to recover remote interpreter process, and skip paragraph recovering.");
+            return new JsonResponse<>(Response.Status.OK, "ERROR").build();
+        }
+
+        paragraph.recover(); // Only recover when remote process recovered.
+        return new JsonResponse<>(Response.Status.OK, "OK").build();
+    }
+
+    private RemoteInterpreterProcess getRemoteInterpreterProcess(Interpreter interpreter) throws IOException {
+        if (interpreter == null) {
+            return null;
+        }
+
+        if (interpreter.getInterpreterGroup() == null) {
+            return null;
+        }
+
+
+        if (!(interpreter.getInterpreterGroup() instanceof ManagedInterpreterGroup)) {
+            return null;
+        }
+
+        ManagedInterpreterGroup interpreterGroup = (ManagedInterpreterGroup) interpreter.getInterpreterGroup();
+        return interpreterGroup.getOrCreateInterpreterProcess(interpreter.getUserName(), interpreter.getProperties());
+    }
+
+    private Paragraph getParagraph(String noteId, String paragraphId) throws IOException {
+        Note note = notebook.getNote(noteId);
+        checkIfNoteIsNotNull(note, noteId);
+        checkIfUserCanRead(noteId);
+
+        Paragraph paragraph = note.getParagraph(paragraphId);
+        checkIfParagraphIsNotNull(paragraph, paragraphId);
+
+        return paragraph;
     }
 
     private void checkIfNoteIsNotNull(Note note, String noteId) {
